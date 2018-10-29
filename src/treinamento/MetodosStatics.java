a74b9b58ee7b62ec242d14f1fb7c03bd5dca22eb
Mauricio <mauricio.nq@hotmail.com>
2018-10-29 20:28:25 -0300

    initial state

---
+++ src/treinamento/MetodosStatics.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package treinamento;
+/**
+ *
+ * @author mauricio
+ */
+public class MetodosStatics {    
+    public static void display() {
+        System.out.println("Jesus te ama");
+    }
+    
+    public static void display(String x) {
+        System.out.println(x);
+    };
+    
+    public static int soma(int x, int y) {
+        return x + y;
+    }
+    
+}
---
+++ src/treinamento/Treinamento.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package treinamento;
+
+/**
+ *
+ * @author mauricio
+ */
+public class Treinamento {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MetodosStatics.display();
+        MetodosStatics.display("CRISTO");
+        System.out.println(MetodosStatics.soma(10, 5));
+    }
+    
+}
